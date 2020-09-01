4a16bbc1fea00675d126ed9a767777e21431d8bf
qiduntest <qidun@magic-shield.com>
2020-09-01 08:57:30 +0000

    调整目录结构

---
+++ src/twosum0001/twosum_model.rs
@@ -0,0 +1,30 @@
+// trait Solution{
+//      fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> ;
+// }
+
+// use std::collections::HashMap;
+// 调试宏  之后在 println 和 print 宏中就可以用 {:?} 占位符输出一整个结构体
+#[derive(Debug)]
+pub struct Solutions {
+    // pub temp: i32,
+}
+
+use std::collections::HashMap;
+impl Solutions {
+    // pub fn new() -> Self {
+    //     // That means the user needs to use new to create a Billy. The user can only input times_to_print
+    //     Self { temp: 3 }
+    // }
+    pub fn two_sum(&self,nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for (i, v) in nums.iter().enumerate() {
+            match map.get(v) {
+                Some(&index) => return vec![index, i as i32],
+                _ => {
+                    map.insert(target - v, i as i32);
+                }
+            }
+        }
+        vec![]
+    }
+}
