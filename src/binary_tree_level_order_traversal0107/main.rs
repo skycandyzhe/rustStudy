076f8106764ff7a6848494a7251b6aed94f7f00b
byy <664346241@qq.com>
2020-09-06 15:06:33 +0800

    0107 等先学RC和RefCell

---
+++ src/binary_tree_level_order_traversal0107/main.rs
@@ -0,0 +1,31 @@
+mod number_of_islands;
+pub use number_of_islands::Solutions;
+
+fn test1() {
+    let mut v = Vec::new();
+
+    let v1 = vec!['1', '1', '0', '1', '0'];
+    v.push(v1);
+    let v1 = vec!['1', '1', '0', '1', '0'];
+    v.push(v1);
+    let v1 = vec!['0', '0', '1', '0', '0'];
+    v.push(v1);
+    let v1 = vec!['0', '0', '0', '1', '1'];
+    v.push(v1);
+    let sol = Solutions {};
+    let ret = sol.num_islands(v);
+    println!("{}", ret);
+    // 输出: 1
+}
+fn test2() {
+    let v: Vec<Vec<char>> = Vec::new();
+    let sol = Solutions {};
+    let ret = sol.num_islands(v);
+    println!("{}", ret);
+    // 输出: 1
+}
+fn main() {
+    test1();
+    test2();
+    // println!("Hello, world!");
+}
