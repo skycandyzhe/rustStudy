c204dadcf4752d75950bb946d3de523dc9f5d141
byy <664346241@qq.com>
2020-09-10 21:03:32 +0800

    add 40 and ac

---
+++ src/combination_sum0040/combination_sum.rs
@@ -0,0 +1,61 @@
+// trait Solution{
+//      fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> ;
+// }
+
+// 给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+// candidates 中的每个数字在每个组合中只能使用一次。
+// 说明：
+// 所有数字（包括目标数）都是正整数。
+// 解集不能包含重复的组合。
+
+//与39类似 但是注意只能用一次 且不能包含重复的组合
+
+
+// use std::rc::Rc;
+// use std::cell::RefCell;
+// use std::collections::HashMap;
+// use std::collections::BinaryHeap;
+#[derive(Debug)]
+pub struct Solution {}
+impl Solution {
+    pub fn combination_recursion(
+        candidates: &Vec<i32>,
+        mut index: i32,
+        target: i32,
+    ) -> Vec<Vec<i32>> {
+        let mut ret: Vec<Vec<i32>> = Vec::new();
+        //     if target>candidates[index]{
+        //        let rettemp=combination_recursion(candidates,index,target-candidates[index]);
+        //    }
+        while index >= 0 {
+            //弹性 先获取最大的值 然后剩下的数据 再取获取
+            let tv = candidates[index as usize];
+            if target > tv {
+                let rettemp = Solution::combination_recursion(candidates, index-1, target - tv);
+                for item in rettemp.iter() {
+                    let mut rett1: Vec<Vec<i32>> = Vec::new();
+                    rett1.push(item.to_vec());
+                    rett1.push(vec![tv]);
+                    let t1 = rett1.concat();
+                    ret.push(t1);
+                }
+            } else if target == tv {
+                ret.push(vec![tv]);
+            }
+            // let rettemp=combination_recursion(candidates,index,target-candidates[index]);
+            index -= 1;
+        }
+        ret
+    }
+    pub fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        // let mut ret:Vec<Vec<i32>>= Vec::new();
+        let mut value: Vec<i32> = candidates.clone();
+        value.sort();
+        // value.sort_by(|a, b| b.cmp(a));
+        // println!("candidates {:?}",value);
+        let mut ret = Solution::combination_recursion(&value, (value.len() - 1) as i32, target);
+        ret.sort();
+        ret.dedup();
+        ret
+    }
+}
---
+++ src/combination_sum0040/main.rs
@@ -0,0 +1,52 @@
+mod combination_sum;
+pub use combination_sum::Solution;
+// use std::rc::Rc;
+// use std::cell::RefCell;
+
+// 1 <= candidates.length <= 30
+// 1 <= candidates[i] <= 200
+// candidate 中的每个元素都是独一无二的。
+// 1 <= target <= 500
+// 最大只有500 用贪心算法 
+
+// 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+// candidates 中的数字可以无限制重复被选取。
+
+// 说明：
+
+// 所有数字（包括 target）都是正整数。
+// 解集不能包含重复的组合。 
+fn test1() {
+    let candidates =vec![10,1,2,7,6,1,5];
+    let k=8;
+    let ret=Solution::combination_sum(candidates,k);
+    println!("{:?}",ret);
+}
+fn test2() {
+    let candidates =vec![2,5,2,1,2,1,1,1,4,2,3];
+    let k=15;
+    let ret=Solution::combination_sum(candidates,k);
+    println!("{:?} {:?}",ret,ret.len());
+}
+fn test3() {
+    let candidates =vec![2,3,6,7,11];
+    let k=11;
+    let ret=Solution::combination_sum(candidates,k);
+    println!("{:?}",ret);
+}
+fn test4() {
+    let candidates =vec![1,2,3,6,7];
+    let k=7;
+    let ret=Solution::combination_sum(candidates,k);
+    println!("{:?}",ret);
+}
+
+fn main() {
+    test1();
+    test2();
+    test3();
+    test4();
+    // test5();
+    // println!("Hello, world!");
+}
