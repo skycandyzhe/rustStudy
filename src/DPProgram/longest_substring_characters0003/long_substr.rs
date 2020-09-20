300204ce20502e196784a60edd729b85597f6b57
byy <664346241@qq.com>
2020-09-20 12:24:37 +0800

    ac0003

---
+++ src/DPProgram/longest_substring_characters0003/long_substr.rs
@@ -0,0 +1,32 @@
+// trait Solution{
+//      fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> ;
+// }
+
+// use std::collections::HashMap;
+// 调试宏  之后在 println 和 print 宏中就可以用 {:?} 占位符输出一整个结构体
+use std::cmp;
+pub struct Solution{}
+impl Solution{
+    pub fn length_of_longest_substring(s: String) -> i32 {
+
+        let mut  char_vec:Vec<i32>=Vec::new(); //上一次字母出现位置 默认为-1
+        char_vec.resize(129, -1);
+        // let slen=s.len();
+        let mut chars = s.chars();
+        let mut index=0;
+        let mut begin=0; //从该位置 到最后没有重复字符
+        let mut contents=chars.next();
+        let mut ret=0;
+        while contents.is_some(){
+            let charnum:usize=contents.unwrap() as usize;
+            if char_vec[charnum]!=-1{//前方存在相同字符
+                begin=cmp::max(begin,char_vec[charnum]+1);//将开始坐标移动到相同字符的右边
+            }
+            contents=chars.next();
+            ret=cmp::max(ret,index-begin+1);
+            char_vec[charnum]=index; //下一次出现改字符  长度 为index-char_vec[charnum]   abcdb 5-2
+            index+=1;
+        }
+        ret
+    }
+}
---
+++ src/DPProgram/longest_substring_characters0003/main.rs
@@ -0,0 +1,20 @@
+mod long_substr;
+pub use long_substr::Solution;
+
+//循环一遍就可以了
+fn test1() {
+    let str1=String::from("abcabcbb");
+    let sol = Solution::length_of_longest_substring(str1);
+    println!("{}", sol);
+}
+fn test2() {
+    let str1=String::from("bbbbddsfsdfdsfsdb");
+    let sol = Solution::length_of_longest_substring(str1);
+    println!("{}", sol);
+    // 输出: 1
+}
+fn main() {
+    test1();
+    test2();
+    // println!("Hello, world!");
+}
